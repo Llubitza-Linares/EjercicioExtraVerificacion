fb10d6e2f96a0b0ab32e006810e9984b12f46dba
Llubitza-Linares <llubylinares@gmail.com>
2022-03-09 22:00:49 -0400

    Ejercicio Extra

---
+++ src/test/java/factoryBrowser/Edge.java
@@ -0,0 +1,9 @@
+package factoryBrowser;
+import org.openqa.selenium.WebDriver;
+
+public class Edge implements IBrowser{
+    @Override
+    public WebDriver createBrowser() {
+        return null;
+    }
+}
